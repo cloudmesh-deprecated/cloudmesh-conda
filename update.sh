185c077273ac66407abcf91048f35eae445b2bfb
Gregor von Laszewski <laszewski@gmail.com>
2019-04-25 20:53:35 -0400

    update update

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
 
-cd .. && cloudmesh-installer git pull cms \
-         cloudmesh-installer git pull conda
+cd ..
 
+cloudmesh-installer git pull cms
+cloudmesh-installer git pull conda
 
-cd cloudmesh-conda && conda build cloudmesh-common && cd ..
-cd cloudmesh-conda && conda build cloudmesh-cmd5 && cd ..
-cd cloudmesh-conda && conda build cloudmesh-sys && cd ..
+cd cloudmesh-conda
+
+conda build cloudmesh-common
+conda build cloudmesh-cmd5
+conda build cloudmesh-sys
